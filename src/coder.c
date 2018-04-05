1b8f5d55489af9a5a3a8f7571e0de1e243431e8d
Kirill <nagavi25599@gmail.com>
2018-04-05 19:39:19 +0700

    Add read_next_code function

--- src/coder.c
+++ src/coder.c
@@ -3,6 +3,8 @@
 #include "coder.h"
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+
 
 void print_code (CodeUnits *code_units)
 {
@@ -60,19 +62,23 @@ int encode (uint32_t code_point, CodeUnits *code_units)
 {
 
 	int sbit = 0;
-	int j = 0;
+	int pos = 0;
+	code_units->length = 0;
+	for (int i = 0; i < MaxCodeLength; i++) {
+		code_units->code [i] = code_units->code [i] & 0;
+	}
 
 	for (uint32_t i = 1; i != 0; i = i << 1){
-		j++;
+		pos++;
 
 		if ((code_point & i) != 0) {
-			sbit = j;
+			sbit = pos;
 		}
+	}
 
-		if (sbit > 21) {
-			printf (ERROR "Value is too large\n" END);
-			return -1;
-		}
+	if (sbit > 21) {
+		printf (ERROR "Value is too large\n" END);
+		return -1;
 	}
 
 	if (sbit <= 7) {
@@ -101,6 +107,224 @@ int encode (uint32_t code_point, CodeUnits *code_units)
 		code_units->code[3] = (code_point & 0x3F ) | 0x80;
 	}
 
-
 	return 0;
+}
+
+int read_next_code_unit (FILE *in, CodeUnits *code_units) {
+
+	int exflag = -1;
+
+	if (exflag == -1) {
+		char path [255];
+    	char lnk [255];
+    	int fno;
+    	ssize_t r;
+
+    	if (in != NULL) {
+    		fno = fileno(in);
+
+        	sprintf(lnk, "/proc/self/fd/%d", fno);
+
+        	r = readlink(lnk, path, 255);
+
+    		if (r < 0) {
+            	printf("failed to readlink\n");
+            	return -1;
+        	}
+
+        	path[r] = '\0';
+    	}
+
+    	if (strstr (path, ".txt\0") != NULL) {
+			exflag = 1;
+		} else if (strstr (path, ".bin\0") != NULL) {
+			exflag = 0;
+		} else {
+    		return -1;
+		}
+	}
+
+	if (exflag == 1) {
+
+		char ch;
+
+		do {
+			ch = getc (in);
+
+			if (ch == EOF) {
+				return -2;
+			}
+		} while (ch == ' ' || ch == '\n');
+
+		if (fseek (in, -1, SEEK_CUR)) {
+			return -1;
+		}
+
+		char code [8];
+		uint32_t num;
+
+		if (fgets (code, 8, in) == NULL) {
+			if (feof (in)) {
+				return -2;
+			}
+
+			if (ferror (in)) {
+				return -1;
+			}
+		}
+
+		num = strtol (code, NULL, 16);
+
+		if (encode (num, code_units)) {
+			return -1;
+		}
+
+		return 0;
+	}
+
+	if (exflag == 0) {
+
+		CodeUnits *form = calloc (sizeof (CodeUnits), 1);
+		if (form == NULL) {
+			return -1;
+		}
+
+		uint8_t *byte = calloc (sizeof (uint8_t), 1);
+
+		read: if (fread (byte, sizeof (uint8_t), 1, in) != 1) {
+			if (feof (in)) {
+				free (byte);
+				free (form);
+				return -2;
+			}
+
+			if (ferror (in)) {
+				free (byte);
+				free (form);
+				return -1;
+			}
+		}
+
+		if ((*byte & 0xc0) == 0x80) {
+			goto read;
+		}
+
+		if ((*byte & 0x80) == 0) {
+			form->code [0] = *byte;
+			form->length = 1;
+		} else if ((*byte & 0xe0) == 0xc0) {
+			form->code [0] = *byte;
+			form->length = 2;
+
+			uint8_t *cont = calloc (sizeof (uint8_t), 1);
+			if (cont == NULL) {
+				return -1;
+			}
+
+			for (int i = 1; i < form->length; i++) {
+				if (fread (cont, sizeof (uint8_t), 1, in) != 1) {
+					if (feof (in)) {
+						free (byte);
+						free (form);
+						return -2;
+					}
+					if (ferror (in)) {
+						free (byte);
+						free (form);
+						return -1;
+					}
+				}
+
+				if ((*cont & 0xc0) == 0x80) {
+					form->code [i] = *cont;
+				} else {
+					if (fseek (in, -1, SEEK_CUR)) {
+						free (byte);
+						free (form);
+						return -1;
+					}
+					goto read;
+				}
+			}
+		} else if ((*byte & 0xf0) == 0xe0) {
+			form->code [0] = *byte;
+			form->length = 3;
+
+			uint8_t *cont = calloc (sizeof (uint8_t), 1);
+			if (cont == NULL) {
+				return -1;
+			}
+
+			for (int i = 1; i < form->length; i++) {
+				if (fread (cont, sizeof (uint8_t), 1, in) != 1) {
+					if (feof (in)) {
+						free (byte);
+						free (form);
+						return -2;
+					}
+					if (ferror (in)) {
+						free (byte);
+						free (form);
+						return -1;
+					}
+				}
+
+				if ((*cont & 0xc0) == 0x80) {
+					form->code [i] = *cont;
+				} else {
+					if (fseek (in, -1, SEEK_CUR)) {
+						free (byte);
+						free (form);
+						return -1;
+					}
+					goto read;
+				}
+			}
+		} else if ((*byte & 0xf8) == 0xf0) {
+			form->code [0] = *byte;
+			form->length = 4;
+
+			uint8_t *cont = calloc (sizeof (uint8_t), 1);
+			if (cont == NULL) {
+				return -1;
+			}
+
+			for (int i = 1; i < form->length; i++) {
+				if (fread (cont, sizeof (uint8_t), 1, in) != 1) {
+					if (feof (in)) {
+						free (byte);
+						free (form);
+						return -2;
+					}
+					if (ferror (in)) {
+						free (byte);
+						free (form);
+						return -1;
+					}
+				}
+
+				if ((*cont & 0xc0) == 0x80) {
+					form->code [i] = *cont;
+				} else {
+					if (fseek (in, -1, SEEK_CUR)) {
+						free (byte);
+						free (form);
+						return -1;
+					}
+					goto read;
+				}
+			}
+		}
+
+		for (int i = 0; i < MaxCodeLength; i++) {
+			code_units->code[i] = form->code[i];
+		}
+		code_units->length = form->length;
+
+		free (form);
+		free (byte);
+		return 0;
+	}
+
+	return -1;
 }
--- src/main.c
+++ src/main.c
@@ -6,6 +6,64 @@
 int main (){
 	system ("clear");
 
+	CodeUnits *b = calloc (sizeof (CodeUnits), 1);
+	if (b == NULL) {
+		printf (ERROR "Error of initialization\n" END);
+		return -1;
+	}
+
+	FILE *inb;
+	FILE *intext;
+
+	inb = fopen ("out.bin", "rb");
+	if (inb == NULL) {
+		printf (ERROR "Can't open input file(BIN)\n" END);
+		return -1;
+	}
+
+	intext = fopen ("testin.txt", "rt");
+	if (intext == NULL) {
+		printf (ERROR "Can't open input file(TXT)\n" END);
+		return -1;
+	}
+
+	printf(INFO "\t\n***BIN***\n\n" END);
+
+	for (int a = 0;;) {
+		a = read_next_code_unit (inb, b);
+		if (a == 0) {
+			print_code (b);
+		} else if (a == -1) {
+			printf(ERROR "ERROR BIN\n" END);
+			break;
+		} else if (a == -2) {
+			printf(INFO "EOF OF BIN\n" END);
+			break;
+		}
+	}
+
+	printf(INFO "\t\n***TEXT***\n\n" END);
+
+	for (int a = 0;;) {
+		a = read_next_code_unit (intext, b);
+		if (a == 0) {
+			print_code (b);
+		} else if (a == -1) {
+			printf(ERROR "ERROR BIN\n" END);
+			break;
+		} else if (a == -2) {
+			printf(INFO "EOF OF BIN\n" END);
+			break;
+		}
+	}
+
+	if (fclose (inb)) {
+		printf (ERROR "Can't properly close input file(BIN)\n" END);
+	}
+
+	if (fclose (intext)) {
+		printf (ERROR "Can't properly close input file(TXT)\n" END);
+	}
 
 	return 0;
 }
